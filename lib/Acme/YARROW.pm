d6d16090700aab536c605428eb762a281eec35f3
John E Fritz <yarrow@cpan.org>
2012-06-13 20:17:23 +0000

    Import of YARROW/Acme-YARROW-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-YARROW
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         YARROW/Acme-YARROW-0.02.tar.gz
    gitpan-cpan-author:       YARROW
    gitpan-cpan-maturity:     released

--- lib/Acme/YARROW.pm
+++ lib/Acme/YARROW.pm
@@ -1,6 +1,6 @@
 package Acme::YARROW;
 
-use 5.006;
+use 5.006
 use strict;
 use warnings;
 
@@ -10,11 +10,11 @@ Acme::YARROW - The great new Acme::YARROW!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
